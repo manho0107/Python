f649eca0702d565effcd4519271de8b893036799
Felix Cheung <81549914+manho0107@users.noreply.github.com>
2023-02-24 01:41:49 -0700

    Delete Lab5_Q6.py

--- Lab5_Q6.py
+++
@@ -1,8 +0,0 @@
-ori_word = [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d')]
-new_word = list()
-
-for word in ori_word:
-    if len(word) >= 1:
-        newword = new_word.append(word)
-
-print(new_word)
